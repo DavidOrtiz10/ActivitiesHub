dee306aa7d1c7bffba8f7399d15547b462a6b2db
David Ortiz <angeldavidortizquiroz@yahoo.com.mx>
2026-09-22 19:45:10 -0600

    feat(parcial02) - add CORS

--- src/EventsHub.Api/Program.cs
+++ src/EventsHub.Api/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt => opt
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 if (app.Environment.IsDevelopment()) { }
 
 using var scope = app.Services.CreateScope();
@@ -23,7 +29,7 @@ try
     await context.Database.MigrateAsync();
     await DbInitializer.SeedDataAsync(context);
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occurred during migration");
--- web/EventsHub/src/App.tsx
+++ web/EventsHub/src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h3>Events Hub</h3>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/v1/events")
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      <h3 style={{ color: "red" }}>Events Hub</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
